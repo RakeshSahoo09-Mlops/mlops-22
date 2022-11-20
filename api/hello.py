a6d05cf71e5bed3a60b6cc99a2745e96ec75ab0d
RakeshSahoo09-Mlops <111623135+RakeshSahoo09-Mlops@users.noreply.github.com>
2022-11-20 09:08:27 +0530

    Update hello.py
    updated code

--- api/hello.py
+++ api/hello.py
@@ -1,24 +1,44 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 from joblib import load
 
+
+
+
 app = Flask(__name__)
-best_model = "svm_gamma=0.0005_C=7.joblib"
-model = load(best_model)
+model_path = "svm_gamma=0.0005_C=5.joblib"
+model = load(model_path)
 
-def predict():
-    content = request.json
-    img1 = content['image1']
-    img2 = content['image2']
-    predicted_digit_1 = best_model.predict([img1])
-    predicted_digit_2 = best_model.predict([img2])
-    if predicted_digit_1 == predicted_digit_2:
-        is_same = True
-    else:
-        is_same = False
-    return jsonify({"predicted_digit_1":str(predicted_digit_1[0]),
-                    "predicted_digit_2":str(predicted_digit_2[0]),
-                    "is_image_same":is_same})
+@app.route("/")
+def hello_world():
+    return "<p><i>Hello, World!</i></p>"
 
-if __name__ == "__main__":
-    app.run(port=5000)
+
+# get x and y somehow
+#     - query parameter
+#     - get call / methods
+#     - post call / methods
+
+
+
+@app.route("/sum", methods=['POST'])
+def sum():
+    print(request.json)
+    x = request.json['x']
+    y = request.json['y']
+    z= x + y
+    return jsonify({'sum': z})
+
+
+    #return -1
+    # z = x + y
+    # return z
+
+@app.route("/predict", methods=['POST'])
+def predict_digit():
+    image = request.json['image']
+    print("done loading")
+    predicted = model.predict([image])
+    return {"y_predicted":int(predicted[0])}
+
