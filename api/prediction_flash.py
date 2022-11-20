1679b5418e93addd2ced548c38efc43aaad31ab4
RakeshSahoo09-Mlops <sahoo.9@iitj.ac.in>
2022-11-20 17:09:14 +0530

    pushed

--- api/prediction_flash.py
+++ api/prediction_flash.py
@@ -5,7 +5,6 @@ from joblib import load
 
 
 
-
 app = Flask(__name__)
 model_path = "svm_gamma=0.0005_C=5.joblib"
 model = load(model_path)
@@ -13,12 +12,6 @@ model = load(model_path)
 @app.route("/")
 def hello_world():
     return "<p><i>Hello, World!</i></p>"
-
-
-# get x and y somehow
-#     - query parameter
-#     - get call / methods
-#     - post call / methods
 
 
 
@@ -31,9 +24,6 @@ def sum():
     return jsonify({'sum': z})
 
 
-    #return -1
-    # z = x + y
-    # return z
 
 @app.route("/predict", methods=['POST'])
 def predict_digit():
